56d8224046c18a88a2091c8ec8c2fca9d559e24d
MarcoValtanen <marco@valtanen.se>
2026-09-10 21:56:14 +0200

    Changes in execution order of code

--- ChessBoard/Program.cs
+++ ChessBoard/Program.cs
@@ -22,7 +22,7 @@ namespace ChessBoard
             }
             while (sizeNumeric < 1 || sizeNumeric > 10);
             
-            Console.WriteLine("Vill du placera en ♕ på Schackbrädan? Ange kordinater: ex. E6");
+            Console.WriteLine("Vill du placera en ♕ på Schackbrädan? Ange kordinater: ex. E6.  (Giltiga värden A-H och 1-10)" );
             string pieceCoordinates = Console.ReadLine();
             
             // Parse coordinate: letter → column index, digit → row number
